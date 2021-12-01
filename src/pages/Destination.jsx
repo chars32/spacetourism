76e152efe76d5e1397a1f6e5951e1fb8acb224d7
chars24@gmail.com <chars24@gmail.com>
2021-12-01 17:25:34 -0600

    DESTINATION MOBILE DESIGN -- DONE ✔️

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -6,7 +6,6 @@ import moonimg from '../assets/destination/image-moon.png'
 const DestinationMainContainer = styled.div`
   background-image: url(${destinationbg});
   background-size: cover;
-  height: 120%;
   width: 100%;
   display: flex;
   justify-content: center;
@@ -84,6 +83,36 @@ const DividerContainer = styled.div`
   margin-top: 2rem;
 `
 
+const PlanetMetrics = styled.div`
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+  height: 20%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`
+
+const PlanetDistance = styled.div`
+`
+
+const PlanetTravelTime = styled.div`
+  margin-top: 2rem;
+`
+
+const MetricTitle = styled.p`
+  font-family: 'Barlow Condensed', sans-serif;
+  font-size: 0.9rem;
+  text-align: center;
+  letter-spacing: 0.16rem;  
+`
+
+const MetricNumbers = styled.p`
+  font-family: 'Bellefair', serif;
+  font-size: 1.7rem;
+  text-align: center;
+  padding-top: 0.75rem;
+`
+
 const Destination = () => {
   return (
     <DestinationMainContainer>
@@ -94,7 +123,7 @@ const Destination = () => {
         </DestinationInfoTitle>
         <DestinationPlanet src={moonimg} />
         <PlanetMenuContainer>
-          <PlanetMenuItem>MOON</PlanetMenuItem>
+          <PlanetMenuItem onClick={() => console.log('moon')}>MOON</PlanetMenuItem>
           <PlanetMenuItem>MARS</PlanetMenuItem>
           <PlanetMenuItem>EUROPA</PlanetMenuItem>
           <PlanetMenuItem>TITAN</PlanetMenuItem>
@@ -104,6 +133,20 @@ const Destination = () => {
           <PlanetInfo>See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.</PlanetInfo>
         </PlanetDescriptionContainer>
         <DividerContainer />
+        <PlanetMetrics>
+          <PlanetDistance>
+            <MetricTitle>
+              AVG. DISTANCE
+            </MetricTitle>
+            <MetricNumbers>
+              384,400 KM
+            </MetricNumbers>
+          </PlanetDistance>
+          <PlanetTravelTime>
+            <MetricTitle>EST. TRAVEL TIME</MetricTitle>
+            <MetricNumbers>3 DAYS</MetricNumbers>
+          </PlanetTravelTime>
+        </PlanetMetrics>
       </DestinationInfoContainer>
     </DestinationMainContainer>
   )
